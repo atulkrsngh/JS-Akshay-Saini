d33bfc8c731a86fe753a8ccf5d51f1c7b99e42d3
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-12-15 18:11:23 +0530

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,13 +1,17 @@
+// Demonstrating currying and recursion
+
 let sum = (a) => {
-    return b =>{
-        if(b){
+    return b => {
+        if(b)
             return sum(a+b);
-        }
         else
             return a;
     }
 }
 
-let sum1 = a => b => {return b ? sum1(a+b) : a;}
+let sum1 = a => b => { 
+    return b ? sum1(a+b) : a;
+}
 
+console.log(sum(1)(2)(3)(4)()); // 10
 console.log(sum1(1)(2)(3)(4)()); // 10
