8c9bb683cf578ee127b5a1ccb0bacd2db5856a75
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-12-15 17:02:28 +0530

    Update EventBubblingAndCapturing.js

--- EventBubblingAndCapturing.js
+++ EventBubblingAndCapturing.js
@@ -1,24 +1,44 @@
-//This is the method to check the sequence of event handlers in dom tree.
+/*
 
-//Bubbling is inside out.
-//child to parent to grandparent
+This is the method to check the sequence of event handlers in the dom tree.
 
-//Capturing is outside to inside.
-//grandparent to parent to child.
-//capturing also known as trickling.
+1. Capturing Phase (also called trickling):
 
-//trickling down and bubbling up.
+Capturing is from outside to inside.
+grandparent to parent to child.
+capturing also known as trickling.
+The event starts at the root (document) and moves downward to the target element.
+Event listeners set to true (addEventListener(event, callback, true)) will execute in this phase.
 
+2. Bubbling Phase:
 
-//Events Bubbling Out
+Bubbling is inside out.
+child to parent to grandparent
+After reaching the target element, the event "bubbles" back up the DOM tree.
+Event listeners set to false (or default) execute in this phase.
+
+trickling down and bubbling up.
+
+Order of Event Propagation:
+
+Capturing Phase → Target Phase → Bubbling Phase.
+Capturing listeners is executed before bubbling listeners.
+
+<div id="grandparent">
+    <div id="parent">
+        <div id="child"></div>
+    </div>
+</div>
+
+*/
 
 document.querySelector("#grandparent")
     .addEventListener('click', () => {
         console.log("Grand parent clicked!!");
     },true);
 
-    //true by default which means bubbling is going on
-    //false need to give means trickling is going on.
+// True as the third argument: Listens during the capturing or trickling phase.
+// False as the third argument (default): Listens during the bubbling phase.
 
 document.querySelector("#parent")
     .addEventListener('click', () => {
